20255628e6566b45321a76e24296db290ccf7b47
Daniel Vallesteros <danielvallesteros@daniels-mbp.home>
2024-06-09 17:53:40 -0400

    day2 password validator part 2

--- src/day2/password-validator.ts
+++ src/day2/password-validator.ts
@@ -13,9 +13,15 @@ interface PasswordDatabaseRow {
     
 }
 
-function isPasswordDatabaseRowValid(row: PasswordDatabaseRow): boolean{
+export function isPasswordDatabaseRowValidRange(row: PasswordDatabaseRow): boolean{
     const letterCount = row.pass.split('').reduce((total, item)=>(item === row.policy.letter ? total + 1 : total),0)
     return row.policy.min <= letterCount && row.policy.max >= letterCount
+}
+
+export function isPasswordDatabaseRowValidIndexes(row: PasswordDatabaseRow): boolean {
+    const chars = row.pass.split('')
+    return 0 < ((chars[row.policy.min] === row.policy.letter? 1 : 0) ^ (chars[row.policy.max] === row.policy.letter ? 1 : 0)) ;
+
 }
 
 export async function parsePasswordDatabase(pathToFile: string): Promise<PasswordDatabaseRow[]>{
@@ -40,8 +46,8 @@ export async function parsePasswordDatabase(pathToFile: string): Promise<Passwor
     })
 }
 
-export async function countValidPasswords(rows: PasswordDatabaseRow[]){
-    return rows.reduce((total, item) => isPasswordDatabaseRowValid(item) ? total+1 : total,0)
+export async function countValidPasswords(rows: PasswordDatabaseRow[], validator: (row: PasswordDatabaseRow) => boolean){
+    return rows.reduce((total, item) => validator(item) ? total+1 : total,0)
 
 }
 
