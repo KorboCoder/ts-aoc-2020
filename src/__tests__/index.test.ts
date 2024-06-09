58113f8b2a2de74a5273071e4416fd22b5c1e28d
Daniel Vallesteros <8325629+KorboCoder@users.noreply.github.com>
2024-06-09 12:10:59 +0800

    ((--BATMAN--))
    ((--BATMAN--))

---
+++ src/__tests__/index.test.ts
@@ -0,0 +1,8 @@
+
+import { getMessage } from '../index'
+
+describe('getMessage()', () => {
+  it('should return the correct message when called', () => {
+    expect(getMessage()).toBe("Hello World!")
+  })
+})
